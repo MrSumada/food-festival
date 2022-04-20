ab299ba21ca81ab775105f356d1c09734f4462b1
MrSumada <ricksamericana.com>
2022-04-20 02:01:02 -0400

    fix start:dev

--- webpack.config.js - old.js
+++ webpack.config.js - old.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
 
 const config = {
@@ -11,9 +12,9 @@ const config = {
     tickets: './assets/js/tickets.js'
   },
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "[name].bundle.js",
-    publicPath: ''
+    filename: '[name].bundle.js',
+    path: `${__dirname}/dist`,
+    publicPath:``,
   },
   module: {
     rules: [
@@ -46,24 +47,21 @@ const config = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static'
-    }),
-    new WebpackPwaManifest({
-      name: 'Food Event',
-      short_name: 'Foodies',
-      description: 'An app that allows you to view upcoming food events.',
-      start_url: '../index.html',
-      background_color: '#01579b',
-      theme_color: '#ffffff',
-      fingerprints: false,
-      inject: false,
-      icons: [
-        {
-          src: path.resolve('assets/img/icons/icon-512x512.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons')
-        }
-      ]
     })
+    // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
   ],
   mode: 'development'
 };
